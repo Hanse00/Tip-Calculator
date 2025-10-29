5832d0f07387b2358536ff5a8ffff8cd9250e696
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-29 15:26:15 +0000

    Implements basic tip calculation

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World";
+    }
+
+    public Double totalWithTip(Double totalBeforeTip, int tipPercentage) {
+        double totalCost = totalBeforeTip + (totalBeforeTip*tipPercentage/100);
+        return totalCost;
     }
 
     public static void main(String[] args) {
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -8,4 +8,18 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test
+    void calculatesBasedOnPercentage() {
+        App classUnderTest = new App();
+        Double result = classUnderTest.totalWithTip(10.0, 10);
+        assertEquals(10.0 + 1.0, result);
+    }
+
+    @Test
+    void calculatesBasedOnPercentage2() {
+        App classUnderTest = new App();
+        Double result = classUnderTest.totalWithTip(150.0, 15);
+        assertEquals(150.0 + 22.5, result);
+    }
 }
